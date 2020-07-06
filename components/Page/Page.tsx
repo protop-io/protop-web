204ce70d79572b37b6100afcc6b2e39c58f053d7
Jeffery Shivers <jefferyshivers@gmail.com>
2020-07-06 12:39:43 -0400

    oops make http image too

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -37,7 +37,7 @@ export const Page = ({ title, description, children, beforeNavBar }: PageProps) 
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <meta property="og:image" content="https://protop.io/logo_with_blue_background_120x63.png" />
+        <meta property="og:image" content="http://protop.io/logo_with_blue_background_120x63.png" />
         <meta property="og:image:secure" content="https://protop.io/logo_with_blue_background_120x63.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:alt" content="protop logo" />
